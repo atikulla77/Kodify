cceac215dcf4f3dd0f6b99976b7a680f20021bf6
codeWithThunder <atikulla345@gmail.com>
2024-12-09 00:35:02 +0600

    portfolio section upload

--- src/Components/Lodding.js
+++ src/Components/Lodding.js
@@ -6,12 +6,12 @@ function Lodding() {
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
       <div className="w-full h-full absolute left-0 top-0 [@media(min-width:600px)]:flex hidden justify-center items-center text-center z-0">
-        <PuffLoader color="#03c0ff39" size={140} className="" />
+        <PuffLoader color="#03c0ff39" size={140} className="flex justify-center items-center text-center" />
       </div>
       <div className="w-full h-full absolute left-0 top-0 [@media(min-width:600px)]:hidden flex justify-center items-center text-center z-0">
-        <PuffLoader color="#03c0ff39" size={120} className="" />
+        <PuffLoader color="#03c0ff39" size={120} className="flex justify-center items-center text-center" />
       </div>
-      <div className="w-full h-full flex justify-center items-center relative z-10">
+      <div className="w-full h-full absolute left-0 top-0 flex justify-center items-center z-10">
         <img src={logo} className="[@media(min-width:600px)]:w-[70px] w-[55px] mx-auto" />
       </div>
     </div>
